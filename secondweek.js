cb61e2924c8b935ac6317aa2396bbbae6c6d8ae7
hike0701 <hiking74013@gmail.com>
2024-01-27 14:56:42 +0800

    第三週

--- secondweek.js
+++ secondweek.js
@@ -77,15 +77,141 @@ const products = [
     </template>
     <p class="text-secondary" v-show="!hasFocus">請選擇一個商品查看</p>
   </div>`
+
+  const productModal = `<!-- Modal -->
+  <div id="product-modal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
+  aria-hidden="true">
+<div class="modal-dialog modal-xl">
+  <div class="modal-content border-0">
+    <div class="modal-header bg-dark text-white">
+      <h5 id="productModalLabel" class="modal-title">
+        <span>新增產品</span>
+      </h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="mb-2">
+            <div class="mb-3">
+              <label for="imageUrl" class="form-label">輸入圖片網址</label>
+              <input type="text" class="form-control"
+                    placeholder="請輸入圖片連結" v-model="product.imageUrl">
+            </div>
+            <img class="img-fluid" src="" alt="">
+          </div>
+          <div>
+            <button class="btn btn-outline-primary btn-sm d-block w-100">
+              新增圖片
+            </button>
+          </div>
+          <div>
+            <button class="btn btn-outline-danger btn-sm d-block w-100">
+              刪除圖片
+            </button>
+          </div>
+        </div>
+        <div class="col-sm-8">
+          <div class="mb-3">
+            <label for="title" class="form-label">標題</label>
+            <input id="title" type="text" class="form-control" placeholder="請輸入標題" v-model="product.title">
+          </div>
+
+          <div class="row">
+            <div class="mb-3 col-md-6">
+              <label for="category" class="form-label">分類</label>
+              <input id="category" type="text" class="form-control"
+                    placeholder="請輸入分類" v-model="product.category">
+            </div>
+            <div class="mb-3 col-md-6">
+              <label for="price" class="form-label">單位</label>
+              <input id="unit" type="text" class="form-control" placeholder="請輸入單位" v-model="product.unit">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="mb-3 col-md-6">
+              <label for="origin_price" class="form-label">原價</label>
+              <input id="origin_price" type="number" min="0" class="form-control" placeholder="請輸入原價" v-model.number="product.origin_price">
+            </div>
+            <div class="mb-3 col-md-6">
+              <label for="price" class="form-label">售價</label>
+              <input id="price" type="number" min="0" class="form-control"
+                    placeholder="請輸入售價" v-model.number="product.price">
+            </div>
+          </div>
+          <hr>
+
+          <div class="mb-3">
+            <label for="description" class="form-label">產品描述</label>
+            <textarea id="description" type="text" class="form-control"
+                      placeholder="請輸入產品描述" v-model="product.content">
+            </textarea>
+          </div>
+          <div class="mb-3">
+            <label for="content" class="form-label">說明內容</label>
+            <textarea id="description" type="text" class="form-control"
+                      placeholder="請輸入說明內容" v-model="product.description">
+            </textarea>
+          </div>
+          <div class="mb-3">
+            <div class="form-check">
+              <input id="is_enabled" class="form-check-input" type="checkbox"
+                    :true-value="1" :false-value="0" v-model="product.is_enabled">
+              <label class="form-check-label" for="is_enabled">是否啟用</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal" v-on:click="cancelModal">
+        取消
+      </button>
+      <button type="button" class="btn btn-primary" v-on:click="modalConfirm">
+        確認
+      </button>
+    </div>
+  </div>
+</div>
+  </div> `
+
+  class product {
+    constructor() {
+      this.title = "";
+      this.category = "";
+      this.origin_price = null;
+      this.price = null;
+      this.unit = "";
+      this.description = "";
+      this.content = "";
+      this.is_enabled = 1;
+      this.imageUrl = "";
+      this.imagesUrl = [];
+    }
+  }
+
+  const childComponent = {
+    props: ["person"],
+    template: "#newComponent",
+  };
+  
   
   const app = Vue.createApp({
       data() {
         return {
           products: [],
           focusProduct: null,
-          hasFocus:false
+          hasFocus: false,
+          modalopen: false,
+          isNewModal: false,
+          delModal: null,
+          expectedDeleteId: 0 
         };
-      },
+    },
+    components: {
+      // 'test': childComponent
+    },
       methods: {
         seeDetail(product) {
           this.focusProduct = product;
@@ -107,35 +233,134 @@ const products = [
             await axios.post(url).then((res) => {
                 const { success } = res.data;
                 result = success;
-            }).catch((err) => { console.log(err); });
+            }).catch((err) => { alert(err); });
             return result;
+        },
+        async deleteProduct() {
+          const id = this.expectedDeleteId;
+          const url =  `https://ec-course-api.hexschool.io/v2/api/yangapi/admin/product/${id}`;
+          let result = false;
+          await axios.delete(url).then((res) => {
+              const { success } = res.data;
+            result = success;
+            alert('已刪除商品');            
+          }).catch((err) => { console.log(err); });
+          await this.getProducts();
+          this.delModal.hide();
+          return result;
+        },
+        openDeleteModal(id) {
+          this.expectedDeleteId = id;
+          this.delModal.show();
+        },
+        openNewModal() {
+          this.isNewModal = true;
+          this.modalopen = true;
+        },
+        openEditModal(product) {
+          this.seeDetail(product);
+          this.isNewModal = false;
+          this.modalopen = true;
+        },
+        closeModal() {
+          this.modalopen = false;
+          this.getProducts();
         }
       },
       async mounted() {
-          const cookieValue = document.cookie
-            .split("; ")
-            .find((row) => row.startsWith("hextoken="))
-              ?.split("=")[1];
+        const cookieValue = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("hextoken="))
+            ?.split("=")[1];
         axios.defaults.headers.common.Authorization = cookieValue;          
-          console.log(cookieValue);
-          this.checkAuth().then((res) => {
-              if (res) {
-                  this.getProducts();
-              }
-              else {
-                  alert("請登入");
-                  window.location = "login.html";
-              }
-          });          
+        console.log(cookieValue);
+        this.checkAuth().then((res) => {
+            if (res) {
+                this.getProducts();
+            }
+            else {
+                alert("請登入");
+                window.location = "login.html";
+            }
+        });
+        this.delModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+          keyboard: false,
+        });
       }
     });
   
-  app.component('detail', {
-      name:'detail',
-      props:['product','hasFocus'],
-      methods: {
-      },
-      template: detailHtml
-    })
-  
+app.component('detail', {
+  name: 'detail',
+  props: ['product', 'hasFocus'],
+  methods: {
+  },
+  template: detailHtml
+});
+
+app.component('product-modal', {
+  name:'product-modal',
+  template: productModal,
+  props: ['isopen', 'isnew', 'propitem'],
+  emits: ['close'],
+  data() {
+    return {
+      product: new product(),
+      productModal: null
+    }
+  },
+  methods: {
+    async addProduct() {
+      const url = "https://ec-course-api.hexschool.io/v2/api/yangapi/admin/product";
+      let result = false;
+      const data = {
+        data: this.product        
+      };
+      await axios.post(url, data).then((res) => {
+        const { success } = res.data;
+        result = success;
+        console.log("addProduct", result);
+      }).catch((err) => { alert(err); });
+      return result;
+    },
+    async updateProduct() {
+      const url = `https://ec-course-api.hexschool.io/v2/api/yangapi/admin/product/${this.product.id}`;
+      let result = false;
+      const data = {
+        data: this.product        
+      };
+      await axios.put(url, data).then((res) => {
+        const { success } = res.data;
+        result = success;
+        console.log("update", result);
+      }).catch((err) => { alert(err); });
+      return result;
+    },
+    async modalConfirm() {
+      let result = false;
+      result = this.isnew ? this.addProduct() : this.updateProduct();
+      this.$emit('close');
+      return result;
+    },
+    cancelModal() {
+      this.$emit('close');
+    }
+  },
+  watch: {
+    isopen: function (newValue, rawValue) {
+      console.log(this.isopen);
+      if (newValue) {
+        this.productModal.show();
+      } else {
+        this.productModal.hide();
+      }
+      this.product = this.isnew ? new product() : { ...this.propitem };
+    },
+  },
+  mounted() {
+    this.productModal = new bootstrap.Modal(document.getElementById('product-modal'), {
+      keyboard: false,
+    });
+  }
+});
+
   app.mount('#app');
